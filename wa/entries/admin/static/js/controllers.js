404a331c9a38b8799a153f1048162a86c9c0fae2
laiyonghao <mail@laiyonghao.com>
2014-10-12 01:01:04 +0800

    整理代码，尝试使用ajax获得菜单，简单规划页面。

--- wa/entries/admin/static/js/controllers.js
+++ wa/entries/admin/static/js/controllers.js
@@ -9,3 +9,29 @@ function HeadController($scope) {
         logo : 'http://offlintab.firefoxchina.cn/static/img/search/baidu_web.png'
     }
 }
+
+function HeaderController($scope) {
+    HeadController($scope)
+}
+
+
+function SidebarController($scope, $http) {
+/*    $scope.menu = [
+        {
+            href:'http://baidu.com',
+            name:'baidu'
+        }
+    ]
+*/
+    $http.get('./menu').success(function (data, status, headers, config) {
+        $scope.menu = angular.fromJson(data)
+    })
+}
+
+function ContainerController($scope) {
+
+}
+
+function FooterController($scope) {
+
+}
--- wa/entries/admin/view.py
+++ wa/entries/admin/view.py
@@ -1,15 +1,12 @@
+# -*- coding:utf-8 -*-
+
+from __future__ import unicode_literals
 
 import os
+import json
 
+from flask import Blueprint, redirect, url_for, make_response, render_template
 from flask.views import MethodView
-
-
-from flask import Blueprint, redirect, url_for, make_response
-
-
-from flask import render_template
-
-from wa.forms import LoginForm
 
 from flask.ext.user import current_user, login_required, UserManager, UserMixin, SQLAlchemyAdapter
 
@@ -19,19 +16,24 @@ admin = Blueprint('admin', __name__,
         static_folder='./static',
         template_folder='./templates')
 
-#def render_template(filename, **kw):
-#    content = open(os.path.join(filename)).read()
-#    return make_response(content.format(**kw))
-
-
-
-# The '/' page is accessible to anyone
 @admin.route('/')
 def home_page():
     if current_user.is_authenticated():
-#        return redirect(url_for('admin.profile_page'))
-#        return make_response(open(os.path.join(HERE, 'templates/index.html')).read())
         return render_template('index.html')
-#        return render_template(os.path.join(HERE, 'templates/index.html'),
-#                               controllers_js=url_for('admin.static', filename='js/controllers.js'))
     return redirect(url_for('user.login'))
+
+@admin.route('/menu')
+@login_required
+def menu():
+    m = [
+        {
+            'name':'配置',
+            'href':url_for('admin.config'),
+        }
+    ]
+    return make_response(json.dumps(m))
+
+@admin.route('/config')
+@login_required
+def config():
+    pass
